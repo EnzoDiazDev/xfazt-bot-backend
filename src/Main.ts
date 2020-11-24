37eca4344603fc197914ec1e0e3d5955bdbe5d45
enzodiaz <enzodiazdev@gmail.com>
2020-11-24 16:20:55 -0300

    restructured

--- src/Main.ts
+++ src/Main.ts
@@ -1,10 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 if(process.env.NODE_ENV === "development") require("dotenv").config();
-import lepp from "./server/Server";
+import Lepp from "@lottielabs/lepp";
+import ErrorRoutes from "./utils/ErrorRoutes";
+import API from "./extensions/API";
+
 
 class Main {
+    private static PORT = process.env.PORT as unknown as number || 3000
+    private static lepp = new Lepp(Main.PORT)
+
     public static main():void {
-        lepp.run();
+        this.lepp.use_helmet()
+            .use_bodyparser()
+            .use_morgan("tiny");
+
+        this.lepp.add_extension(API);
+
+        this.lepp.use(ErrorRoutes.error404)
+            .use(ErrorRoutes.error500);
+
+        this.lepp.run();
     }
 }
 
